0610dfcded3392aca98225b36d827a167f1040c8
eduardspahija <eduard-spahija@hotmail.com>
2019-04-18 22:59:52 +0200

    fibonacci ndryshimi

--- FIEK-TCP/FIEK-TCP Serveri.py
+++ FIEK-TCP/FIEK-TCP Serveri.py
@@ -87,8 +87,10 @@ def KONVERTIMI(llojiKonvertimit, vlera):
 def FIBONACCI(n):
     if n<0:
         print("Numri duhet te jete me i madh se zero")
-    elif n == 1:
+    elif n == 0:
         return 0
+    elif n == 1:
+        return 1
     elif n == 2:
         return 1
     else:
